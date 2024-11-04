1bcf80410d81aa2683a16eead502592dc7cf0dd4
carlos <U20201C794@upc.edu.pe>
2024-11-03 19:04:52 -0500

    real backend connection

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment={
     production:false,
-    domain: 'http://localhost:8080/api/',  
+    domain: 'http://eventra-backend-982215-9a4f09-35-239-187-59.traefik.me/api/',  
 }
